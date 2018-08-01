fda0d0244b99e59f8d1f381dbc4aa9af6750eb75
Marcos Scriven <marcos@scriven.org>
2018-08-01 13:58:16 +0100

    Rejig file structure.

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 mkdir -p build
-docker build -t qvmf-vbios-patch .       
+docker build -t qvmf-vbios-patch docker-build       
 docker run -v "$PWD/build:/build" -v "$PWD/roms:/roms" qvmf-vbios-patch /compile-qvmf.sh
 tar -czf build/qvmf-vbios-patced.tgz build/OVMF*
