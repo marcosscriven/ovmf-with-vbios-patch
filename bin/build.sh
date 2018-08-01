60a33eb65a6b44bbc91a37de50aaad83f827301b
Marcos Scriven <marcos@scriven.org>
2018-08-01 17:23:37 +0100

    Fix artifact typo.

--- bin/build.sh
+++ bin/build.sh
@@ -3,4 +3,4 @@
 mkdir -p build
 docker build -t qvmf-vbios-patch docker-build       
 docker run -v "$PWD/build:/build" -v "$PWD/roms:/roms" qvmf-vbios-patch /qvmf/compile-qvmf.sh
-tar -czf build/qvmf-vbios-patced.tgz build/OVMF*
+tar -czf build/qvmf-vbios-patched.tgz build/OVMF*
